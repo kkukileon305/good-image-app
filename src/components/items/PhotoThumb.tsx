8c4ca76f2e418366e7e8b3bbc5efdd0e3e502d94
kkukileon305 <kkukileon305@gmail.com>
2023-01-22 19:17:49 +0900

    photoThumb title 수정

--- src/components/items/PhotoThumb.tsx
+++ src/components/items/PhotoThumb.tsx
@@ -12,7 +12,7 @@ const PhotoThumb = ({ photo }: PhotoThumbProps) => {
       <Link to={`photo/${photo.id}`} className='w-full flex justify-between p-4 shadow-md border rounded-xl overflow-hidden'>
         <img src={photo.urls.thumb} alt={photo.alt_description || 'There is no description...'} />
         <div className='w-[calc(100%-216px)] flex flex-col justify-between'>
-          <p>{photo.description || 'There is no description...'}</p>
+          <p>{photo.alt_description || 'There is no description...'}</p>
           <div className='flex justify-end gap-4 text-gray-400'>
             <p>{krIntl.format(new Date(photo.created_at))}</p>
             <p>{photo.user.name}</p>
