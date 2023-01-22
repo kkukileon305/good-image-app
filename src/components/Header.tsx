7cc6bda280cb2c5379146831c4b928b0cb5ae2a1
kkukileon305 <kkukileon305@gmail.com>
2023-01-22 20:37:27 +0900

    스타일 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 const Header = () => (
   <header className='border-b sticky top-0 bg-white/70 backdrop-blur-sm z-10'>
     <div className='flex items-center max-w-[1440px] w-full mx-auto'>
-      <div className='flex gap-4 items-center w-[200px] p-4'>
+      <div className='flex gap-4 items-center w-[200px] px-4 py-2'>
         <h1 className='text-2xl font-bold'>
           <Link to={'/'}>Good Photo</Link>
         </h1>
       </div>
-      <div className='flex items-center w-[calc(100%-200px)] justify-between gap-4 p-4'>
+      <div className='flex items-center w-[calc(100%-200px)] justify-between gap-4 px-4 py-2'>
         <div className='hidden md:flex bg-gray-300 px-4 py-2 rounded-full items-center'>
           <input type='text' className='bg-transparent' placeholder='Search...' />
           <BsSearch />
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -6,7 +6,7 @@ const Root = () => {
     <>
       <Header />
       <div className='max-w-[1440px] w-full mx-auto flex'>
-        <ul className='hidden lg:block w-[200px] h-[calc(100vh-73px)] border-r p-4 sticky top-[73px]'></ul>
+        <ul className='hidden lg:block w-[200px] h-[calc(100vh-57px)] border-r p-4 sticky top-[57px]'></ul>
         <main className='w-full lg:w-[calc(100%-200px)] p-4'>
           <Outlet />
         </main>
